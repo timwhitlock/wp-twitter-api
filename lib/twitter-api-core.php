7c41d8da69dddb8d27d0d54869276a041b0ce9b4
Tim Whitlock <r-github@timwhitlock.info>
2013-02-28 21:11:00 +0000

    removed http status replication

--- lib/twitter-api-core.php
+++ lib/twitter-api-core.php
@@ -249,7 +249,7 @@ class TwitterApiClient {
         // unserializable array assumed to be serious error
         if( ! is_array($data) ){
             $err = array( 
-                'message' => __($http['response']['message']), 
+                'message' => '', // <- blank so we use twitter-specific message
                 'code' => -1 
             );
             TwitterApiException::chuck( $err, $status );
@@ -455,61 +455,28 @@ class TwitterOAuthParams {
 
 
 /**
- * HTTP status codes with some overridden for Twitter-related messages.
+ * Overridden HTTP status codes for common Twitter-related problems.
  * Note these do not replace error text from Twitter, they're for complete API failures.
  * @param int HTTP status code
  * @return string HTTP status text
  */
 function _twitter_api_http_status_text( $s ){
     static $codes = array (
-        100 => 'Continue',
-        101 => 'Switching Protocols',
-        
-        200 => 'OK',
-        201 => 'Created',
-        202 => 'Accepted',
-        203 => 'Non-Authoritative Information',
-        204 => 'No Content',
-        205 => 'Reset Content',
-        206 => 'Partial Content',
-        
-        300 => 'Multiple Choices',
-        301 => 'Moved Permanently',
-        302 => 'Found',
-        303 => 'See Other',
-        304 => 'Not Modified',
-        305 => 'Use Proxy',
-        307 => 'Temporary Redirect',
-        
-        400 => 'Bad Request',
-        401 => 'Authorization Required',
-        402 => 'Payment Required',
-        403 => 'Forbidden',
-        404 => 'Not Found',
-        405 => 'Method Not Allowed',
-        406 => 'Not Acceptable',
-        407 => 'Proxy Authentication Required',
-        408 => 'Request Time-out',
-        409 => 'Conflict',
-        410 => 'Gone',
-        411 => 'Length Required',
-        412 => 'Precondition Failed',
-        413 => 'Request Entity Too Large',
-        414 => 'Request-URI Too Large',
-        415 => 'Unsupported Media Type',
-        416 => 'Requested range not satisfiable',
-        417 => 'Expectation Failed',
-        //  ..
         429 => 'Twitter API rate limit exceeded',
-        
         500 => 'Twitter server error',
-        501 => 'Not Implemented',
         502 => 'Twitter is not responding',
         503 => 'Twitter is too busy to respond',
-        504 => 'Gateway Time-out',
-        505 => 'HTTP Version not supported',
     );
-    return __( isset($codes[$s]) ? $codes[$s] : sprintf('Status %u from Twitter', $s) );
+    if( isset($codes[$s]) ){
+        return __( $codes[$s] );
+    }
+    // fall back to Wordpress registry to save bloat
+    $text = get_status_header_desc( $s );
+    if( $text ){
+        return __( $text );
+    }
+    // unknown status    
+    return sprintf( __('Status %u from Twitter'), $s );
 }
 
 

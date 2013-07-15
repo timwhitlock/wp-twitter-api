f3b1fc9fd37d4791568bfb36955b4fa11d7548be
Tim Whitlock <r-github@timwhitlock.info>
2013-07-15 21:13:34 +0100

    updated translations

--- lang/update.sh
+++ lang/update.sh
@@ -4,13 +4,13 @@
 # Note that the API key is reset between releases.
 #
 
-APIKEY="535225653ef7eb710e53c8e421e335e1";
+APIKEY="f81c647f83248b38348c23304b6027a4";
 
 cd "`dirname $0`"
 
 function loco {
     if [ "enGB" = "$1$2" ]; then
-        wget "http://localise.biz/api/export/locale/$1-$2.po?key=$APIKEY" -O "twitter-api.pot"
+        wget "http://localise.biz/api/export/locale/$1-$2.pot?key=$APIKEY" -O "twitter-api.pot"
     else
         wget "http://localise.biz/api/export/locale/$1-$2.po?key=$APIKEY" -O "twitter-api-$1_$2.po"
         msgfmt "twitter-api-$1_$2.po" -o "twitter-api-$1_$2.mo"
